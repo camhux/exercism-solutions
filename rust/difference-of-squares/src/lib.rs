242a2e27c70f1f9335785c76b5002b632f15403b
camhux <camzig@gmail.com>
2016-01-29 19:00:55 -0800

    Difference of squares

---
+++ rust/difference-of-squares/src/lib.rs
@@ -0,0 +1,14 @@
+pub fn square_of_sum(n: i64) -> i64 {
+    let sum = (1..(n+1)).fold(0, |acc, x| acc + x);
+    sum * sum
+}
+
+pub fn sum_of_squares(n: i64) -> i64 {
+    (1..(n+1)).map(|x| x * x).fold(0, |acc, x| acc + x)
+}
+
+pub fn difference(n: i64) -> i64 {
+    let s1 = square_of_sum(n);
+    let s2 = sum_of_squares(n);
+    s1 + (-s2)
+}
